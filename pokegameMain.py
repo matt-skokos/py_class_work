59399507c0f1f16ab0f11d3792f41cbb45dfc3b1
Matt <58489693+matt-skokos@users.noreply.github.com>
2022-06-12 17:12:31 -0700

    Update pokegameMain.py

--- pokegameMain.py
+++ pokegameMain.py
@@ -1,19 +1,15 @@
-from pokemon import *
 from PokeGame import PokeGame
 
 
 def main():
-    deck1 = []
-    pokegame1 = PokeGame(deck1)
+    pokegame1 = PokeGame()
     opponent_pokemon = pokegame1.draw_pokemon()
-    try:
-        print(f"Your opponent has selected: {opponent_pokemon.name} \n"
-              f"{opponent_pokemon}")
-        user_pokemon = pokegame1.print_menu()
+    while opponent_pokemon:
+        user_pokemon = pokegame1.get_choice()
         user_pokemon.attack(opponent_pokemon)
-    except AttributeError:
-        print("Thanks for playing!")
+        opponent_pokemon = pokegame1.draw_pokemon()
 
 
 if __name__ == "__main__":
     main()
+
