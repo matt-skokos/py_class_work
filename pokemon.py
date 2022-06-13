274742734fe946ea55b19fd2dae2df96ef2fcdd1
Matt <58489693+matt-skokos@users.noreply.github.com>
2022-06-12 17:14:04 -0700

    Update pokemon.py

--- pokemon.py
+++ pokemon.py
@@ -26,8 +26,8 @@ class Pokemon:
             print(f"{self.name} fainted!")
 
     def __str__(self):
-        return f"{self.name}'s trainer is {self.trainer} and their basic " \
-               f"attack is '{self.basic_attack}'"
+        return f"{self.name}'s trainer is {self.trainer} and their " \
+               f"basic attack is '{self.basic_attack}'"
 
 
 class PsychicType(Pokemon):
@@ -59,8 +59,8 @@ class PsychicType(Pokemon):
             print(f"{self.name} fainted!")
 
     def __str__(self):
-        return f"{self.name}'s trainer is {self.trainer} and their basic " \
-               f"attack is '{self.basic_attack}'"
+        return f"{self.name}'s trainer is {self.trainer} and " \
+               f"their basic attack is '{self.basic_attack}'"
 
 
 # FairyType class by Allison Tsai
@@ -70,7 +70,7 @@ class FairyType(Pokemon):
     prob = .1
 
     def __init__(self, name, trainer, hp: int):
-        super().__init__(name, trainer)  # maybe shoot a message that the parenthesis are missing
+        super().__init__(name, trainer)
         self.level = 1
         self.hp = hp
         self.paralyzed = False
@@ -79,8 +79,8 @@ class FairyType(Pokemon):
         self.poisoned = False
 
     def __str__(self):
-        return f"FairyPokemon:{self.name}:{self.trainer}:{self.level}:" \
-               f"{self.hp}:{self.paralyzed}"
+        return f"FairyPokemon:{self.name}:{self.trainer}:{self.level}" \
+               f": {self.hp}:{self.paralyzed}"
 
     def attack(self, other):
         if not self.paralyzed:
@@ -94,20 +94,21 @@ class FairyType(Pokemon):
                 other.receive_damage(half_damage)
             else:
                 other.receive_damage(self.damage)
-            if random.random() < self.prob:  # chance of inflicting Confusion
+            if random() < self.prob:  # chance of inflicting Confusion
                 other.confused = True
                 print(f"{other.name} is confused!")
         else:
-            print(f"{self.name} could not attack. {self.name} is paralyzed!")
+            print(f"{self.name} could not attack. {self.name} "
+                  f"is paralyzed!")
 
 
 class FightingType(Pokemon):
-    """FightingType stub"""
+    """Strong against"""
     pass
 
 
 class PoisonType(Pokemon):
-    """PoisonType against"""
+    """Strong against"""
     pass
 
 
@@ -137,17 +138,22 @@ class DragonType(Pokemon):
 
 
 def main():
-    necrozma = PsychicType("Necrozma", "Matt")
-    darkpoke = DarkType("DarkPoke", "Dennis")  # weak against check
-    ghostpoke = GhostType("GhostType", "Greg")  # damage received check
-    fightpoke = FightingType("FighterType", "Frank")  # strong against check
-    necrozma.attack(darkpoke)
-    print("Darkpoke hp: ", darkpoke.hp)
-    ghostpoke.attack(necrozma)
-    print("Necrozma hp: ", necrozma.hp)
-    fightpoke.attack(necrozma)
-    darkpoke.attack(necrozma)
-    print("Necrozma hp: ", necrozma.hp)
+    pass
+    '''     --- TEST CASES FOR Pokemon Objects ---       '''
+    #
+    # necrozma = PsychicType("Necrozma", "Matt")
+    # darkpoke = DarkType("DarkPoke", "Dennis")  # weak against check
+    # ghostpoke = GhostType("GhostType", "Greg")  # damage received check
+    # fightpoke = FightingType("FighterType", "Frank")  # strong against check
+    # # necrozma.attack(darkpoke)
+    # print("Darkpoke hp: ", darkpoke.hp)
+    # # ghostpoke.attack(necrozma)
+    # # print("Necrozma hp: ", necrozma.hp)
+    # # fightpoke.attack(necrozma)
+    # # print("Necrozma hp: ", necrozma.hp)
+    # print("Necrozma hp: ", necrozma.hp)
+    # darkpoke.attack(necrozma)
+    # print("Necrozma hp: ", necrozma.hp)
 
 
 if __name__ == "__main__":
